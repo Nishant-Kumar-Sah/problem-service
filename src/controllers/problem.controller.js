316665b692c3909fe269018c4def38bb01737aa5
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2024-12-25 11:40:31 +0530

    not implemented error handling & https status codes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,23 +1,41 @@
+const { StatusCodes}= require('http-status-codes')
+
+
 function pingController(req,res) {
     return res.json({message: "Ping COntroller is Up"})
 }
 
 function addProblem(req, res) {
 
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
+
 }
 
 function getProblem(req, res) {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 }
 
 function getProblems(req, res) {
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 
 }
 
 function deleteProblem(req, res) {
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 
 }
 function updateProblem(req, res){
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 
 }
 
